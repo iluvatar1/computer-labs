54c3ff4292f0507cd7986a6a6626e4fcf0e35a85
Iluvatar <wfoquendop@unal.edu.co>
2017-01-18 15:31:01 -0500

    moved chromium to misc

--- packages/install_packages_slackware.sh
+++ packages/install_packages_slackware.sh
@@ -10,11 +10,11 @@ SBO_CMD="sboinstall -r -j 2"
 PKG=
 SRC=
 if [ "$1" == "BASIC" ]; then
-    PKG="monit fail2ban grace corkscrew djview4 lame  unrar smplayer valgrind valkyrie  chromium libreoffice openmpi blas openblas octave dropbox pip parallel pdftk"
+    PKG="monit fail2ban grace corkscrew djview4 lame  unrar smplayer valgrind valkyrie libreoffice openmpi blas openblas octave dropbox pip parallel pdftk"
 elif [ "$1" == "NUMERIC" ]; then
     PKG="eigen3 fltk netcdf arpack atlas gsl hdf5 lapack suitsparse armadillo openblas"    
 elif [ "$1" == "MISC" ]; then
-    PKG="kile kdenlive filezilla scribus povray texmaker smplayer vlc inkscape ffmpeg audacity graphviz libticonv gocr msmtp lyx geany wine eagle skype twolame mplayer-codecs32 flashplayer-plugin pyserial dfc  wol sshfs-fuse acpica virtualbox-kernel virtualbox-kernel-addons virtualbox-extension-pack paraview  PyYAML  cppcheck iotop ntpclient  proxychains proxytunnel "
+    PKG="kile kdenlive filezilla scribus povray texmaker chromium smplayer vlc inkscape ffmpeg audacity graphviz libticonv gocr msmtp lyx geany wine eagle skype twolame mplayer-codecs32 flashplayer-plugin pyserial dfc  wol sshfs-fuse acpica virtualbox-kernel virtualbox-kernel-addons virtualbox-extension-pack paraview  PyYAML  cppcheck iotop ntpclient  proxychains proxytunnel "
 elif [ "$1" == "EXTRA" ]; then
     PKG="GoogleEarth blender QtiPlot scidavis"
 elif [ "$1" == "SALAFIS" ]; then
