3f0a56f2c22a9d000102e2692ed4d1c0b06ea67b
Iluvatar <wfoquendop@unal.edu.co>
2017-01-17 21:00:53 -0500

    Fixed options for script

--- packages/install_packages_slackware.sh
+++ packages/install_packages_slackware.sh
@@ -1,7 +1,7 @@
 # list of packages to install (common mainly by name)
-if [ "$1" != "BASIC" ] && [ "$1" != "UTIL" ] && [ "$1" != "EXTRA" ] ; then
+if [ "$1" != "BASIC" ] && [ "$1" != "MISC" ] && [ "$1" != "NUMERIC" ] && [ "$1" != "EXTRA" ] && [ "$1" != "SALAFIS" ]; then
     echo "Error. Call this script as :"
-    echo "$0 [BASIC|UTIL|EXTRA]"
+    echo "$0 [BASIC|NUMERIC|MISC|EXTRA|SALAFIS]"
     exit 1
 fi
 
@@ -11,12 +11,18 @@ PKG=
 SRC=
 if [ "$1" == "BASIC" ]; then
     PKG="monit fail2ban grace corkscrew djview4 ffmpeg graphviz inkscape lame  smplayer unrar valgrind valkyrie  chromium libreoffice openmpi octave dropbox vlc scribus audacity armadillo pip parallel pdftk"
+elif [ "$1" == "NUMERIC"]; then
+    PKG="eigen3 fltk netcdf arpack atlas gsl hdf5 lapack suitsparse QtiPlot scidavis"    
+elif [ "$1" == "MISC"]; then
+    PKG="kile kdenlive filezilla povray texmaker libticonv gocr msmtp lyx geany wine eagle skype twolame mplayer-codecs32 flashplayer-plugin pyserial dfc  wol sshfs-fuse acpica virtualbox-kernel virtualbox-kernel-addons virtualbox-extension-pack paraview  PyYAML  cppcheck iotop ntpclient  proxychains proxytunnel "
+elif [ "$1" == "EXTRA"]; then
+    PKG="GoogleEarth blender"
+elif [ "$1" == "SALAFIS"]; then
+    PKG="octave arduino"
 fi
+
 for pkgname in $PKG; do
     $SBO_CMD $pkgname
 done
 		   
-NUMERIC="eigen3 fltk netcdf arpack atlas gsl hdf5 lapack suitsparse QtiPlot scidavis"
 
-UTIL="kile kdenlive filezilla povray texmaker libticonv gocr msmtp lyx geany wine eagle skype twolame mplayer-codecs32 flashplayer-plugin pyserial dfc  wol sshfs-fuse acpica virtualbox-kernel virtualbox-kernel-addons virtualbox-extension-pack paraview  PyYAML  cppcheck iotop ntpclient  proxychains proxytunnel "
-EXTRA="GoogleEarth blender"
