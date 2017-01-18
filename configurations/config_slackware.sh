024ed7adecabd5d7973b86817911f23e7ed29f9b
Iluvatar <wfoquendop@unal.edu.co>
2017-01-17 22:04:10 -0500

    Added code to link xinitrc after slim slection

--- configurations/config_slackware.sh
+++ configurations/config_slackware.sh
@@ -14,6 +14,7 @@ chmod +x /etc/rc.d/rc.nfsd
 echo "Configuring slim login manager"
 if [ x"" == x"$(grep slim /etc/rc.d/rc.4)" ]; then
     sed -i.bck '/echo "Starting up X11 session manager..."/a \\n# start SLiM ...\nif [ -x /usr/bin/slim ]; then exec /usr/bin/slim; fi ' /etc/rc.d/rc.4
+    ln -sf /etc/X11/xinit/xinitrc.xfce /etc/X11/xinitrc
 fi
 
 echo "Done."
