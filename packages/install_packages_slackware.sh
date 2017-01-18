6cf48cda203431fe2fdd5b02c3548a98181d81bb
Iluvatar <wfoquendop@unal.edu.co>
2017-01-18 15:36:45 -0500

    Installed ninja in misc and changed default command in misc to follow requirements

--- packages/install_packages_slackware.sh
+++ packages/install_packages_slackware.sh
@@ -14,7 +14,8 @@ if [ "$1" == "BASIC" ]; then
 elif [ "$1" == "NUMERIC" ]; then
     PKG="eigen3 fltk netcdf arpack atlas gsl hdf5 lapack suitsparse armadillo openblas"    
 elif [ "$1" == "MISC" ]; then
-    PKG="kile kdenlive filezilla scribus povray texmaker chromium smplayer vlc inkscape ffmpeg audacity graphviz libticonv gocr msmtp lyx geany wine eagle skype twolame mplayer-codecs32 flashplayer-plugin pyserial dfc  wol sshfs-fuse acpica virtualbox-kernel virtualbox-kernel-addons virtualbox-extension-pack paraview  PyYAML  cppcheck iotop ntpclient  proxychains proxytunnel "
+    PKG="kile kdenlive filezilla scribus povray texmaker ninja chromium smplayer vlc inkscape ffmpeg audacity graphviz libticonv gocr msmtp lyx geany wine eagle skype twolame mplayer-codecs32 flashplayer-plugin pyserial dfc  wol sshfs-fuse acpica virtualbox-kernel virtualbox-kernel-addons virtualbox-extension-pack paraview  PyYAML  cppcheck iotop ntpclient  proxychains proxytunnel "
+    SBO_CMD="sboinstall -j 2"
 elif [ "$1" == "EXTRA" ]; then
     PKG="GoogleEarth blender QtiPlot scidavis"
 elif [ "$1" == "SALAFIS" ]; then
