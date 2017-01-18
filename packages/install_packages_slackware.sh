c4223bf97fcb7b7dd871b3704e351eada303098d
Iluvatar <wfoquendop@unal.edu.co>
2017-01-18 15:15:14 -0500

    Removed non-interactive flag for sbotools

--- packages/install_packages_slackware.sh
+++ packages/install_packages_slackware.sh
@@ -5,16 +5,16 @@ if [ "$1" != "BASIC" ] && [ "$1" != "MISC" ] && [ "$1" != "NUMERIC" ] && [ "$1" 
     exit 1
 fi
 
-SBO_CMD="sboinstall -r -j 2"
+SBO_CMD="sboinstall -j 2"
 
 PKG=
 SRC=
 if [ "$1" == "BASIC" ]; then
-    PKG="monit fail2ban grace corkscrew djview4 lame  unrar valgrind valkyrie  chromium libreoffice openmpi openblas octave dropbox vlc pip parallel pdftk"
+    PKG="monit fail2ban grace corkscrew djview4 lame  unrar smplayer valgrind valkyrie  chromium libreoffice openmpi openblas octave dropbox pip parallel pdftk"
 elif [ "$1" == "NUMERIC" ]; then
     PKG="eigen3 fltk netcdf arpack atlas gsl hdf5 lapack suitsparse armadillo openblas"    
 elif [ "$1" == "MISC" ]; then
-    PKG="kile kdenlive filezilla scribus povray texmaker smplayer inkscape ffmpeg audacity graphviz libticonv gocr msmtp lyx geany wine eagle skype twolame mplayer-codecs32 flashplayer-plugin pyserial dfc  wol sshfs-fuse acpica virtualbox-kernel virtualbox-kernel-addons virtualbox-extension-pack paraview  PyYAML  cppcheck iotop ntpclient  proxychains proxytunnel "
+    PKG="kile kdenlive filezilla scribus povray texmaker smplayer vlc inkscape ffmpeg audacity graphviz libticonv gocr msmtp lyx geany wine eagle skype twolame mplayer-codecs32 flashplayer-plugin pyserial dfc  wol sshfs-fuse acpica virtualbox-kernel virtualbox-kernel-addons virtualbox-extension-pack paraview  PyYAML  cppcheck iotop ntpclient  proxychains proxytunnel "
 elif [ "$1" == "EXTRA" ]; then
     PKG="GoogleEarth blender QtiPlot scidavis"
 elif [ "$1" == "SALAFIS" ]; then
