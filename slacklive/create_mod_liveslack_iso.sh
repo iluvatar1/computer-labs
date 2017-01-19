739c4aa074111960b2424c413db82d38d14cbbe5
Iluvatar <wfoquendop@unal.edu.co>
2017-01-19 07:53:11 -0500

    minor test

--- slacklive/create_mod_liveslack_iso.sh
+++ slacklive/create_mod_liveslack_iso.sh
@@ -1,9 +1,9 @@
 echo "Creating liveslack iso with custom modules"
-mount -o loop /dev/sr0 /mnt/dvd
-mkdir -p /tmp/slackwarelive_staging
-rsync -av -P /mnt/dvd/ /tmp/slackwarelive_staging/
-umount /mnt/dvd
-cp -avu Downloads/006*sxz /tmp/slackwarelive_staging/liveslak/addons/
-cd /tmp/slackwarelive_staging/
-bash ~/Downloads/liveslak/make_slackware_live.sh -G
+mount -o loop /dev/sr0 /mnt/dvd && 
+mkdir -p /tmp/slackwarelive_staging && 
+rsync -av -P /mnt/dvd/ /tmp/slackwarelive_staging/ && 
+umount /mnt/dvd && 
+cp -avu Downloads/006*sxz /tmp/slackwarelive_staging/liveslak/addons/ &&
+cd /tmp/slackwarelive_staging/ &&
+bash ~/Downloads/liveslak/make_slackware_live.sh -G && 
 echo "Done. Copy the iso where you need it."
