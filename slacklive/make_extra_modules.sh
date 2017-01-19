ea3cd5fe9fd7c54f81d113a27ecad0b1c5623ee0
Iluvatar <wfoquendop@unal.edu.co>
2017-01-18 20:45:27 -0500

    Added a script to create the anaconda and valgrind modules for liveslak

---
+++ slacklive/make_extra_modules.sh
@@ -0,0 +1,36 @@
+echo "#####################################"
+echo "CREATING anaconda MODULE ... "
+if [ ! -d /tmp/modtemp/opt ]; then 
+    mkdir -p /tmp/modtemp/opt
+fi
+echo "Copying anaconda2 to /tmp/modtemp/opt/ ..."
+cp -auvf /opt/anaconda2 /tmp/modtemp/opt/
+echo "Creating a local profile to modify the path for all users ... "
+mkdir -p /tmp/modtemp/etc/profile.d
+echo 'export PATH="/opt/anaconda2/bin:$PATH"' > /tmp/modtemp/etc/profile.d/anaconda.sh
+chmod -x /tmp/modtemp/etc/profile.d/anaconda.sh
+echo "Creating liveslack module ... "
+bash liveslak/makemod.sh -i  /tmp/modtemp/ 0066-anaconda2-4.2.0-x86_64.sxz
+echo "Done anaconda module."
+echo "You can test the module contents with the command : "
+echo "unsquashfs -l 0066-anaconda2-4.2.0-x86_64.sxz"
+echo "#####################################"
+echo 
+
+echo "#####################################"
+echo "CREATING valgrind MODULE"
+mkdir -p /tmp/valgrind
+cd /tmp/valgrind
+wget -c https://slackbuilds.org/slackbuilds/14.2/development/valgrind.tar.gz
+tar xf valgrind.tar.gz
+cd valgrind
+wget -c http://www.valgrind.org/downloads/valgrind-3.11.0.tar.bz2
+bash valgrind.SlackBuild
+cd
+bash liveslak/makemod -i /tmp/valgrind-3.11.0-x86_64-1_SBo.tgz 0067-valgrind-3.11.0-x86_64-1_SBo.sxz
+echo "Done valgrind module."
+echo "You can test the module contents with the command : "
+echo "unsquashfs -l 0067-valgrind-3.11.0-x86_64-1_SBo.sxz"
+echo "#####################################"
+echo 
+
