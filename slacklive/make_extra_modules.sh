8cc9da0356f26027aca9504b244bcf77bb9a08c9
Iluvatar <wfoquendop@unal.edu.co>
2017-01-18 22:19:52 -0500

    Updated makemod name

--- slacklive/make_extra_modules.sh
+++ slacklive/make_extra_modules.sh
@@ -1,3 +1,5 @@
+MAKEMOD=makemod
+
 echo "#####################################"
 echo "CREATING anaconda MODULE ... "
 if [ ! -d /tmp/modtemp/opt ]; then 
@@ -10,7 +12,7 @@ mkdir -p /tmp/modtemp/etc/profile.d
 echo 'export PATH="/opt/anaconda2/bin:$PATH"' > /tmp/modtemp/etc/profile.d/anaconda.sh
 chmod -x /tmp/modtemp/etc/profile.d/anaconda.sh
 echo "Creating liveslack module ... "
-bash liveslak/makemod.sh -i  /tmp/modtemp/ 0066-anaconda2-4.2.0-x86_64.sxz
+bash liveslak/$MAKEMOD -i  /tmp/modtemp/ 0066-anaconda2-4.2.0-x86_64.sxz
 echo "Done anaconda module."
 echo "You can test the module contents with the command : "
 echo "unsquashfs -l 0066-anaconda2-4.2.0-x86_64.sxz"
@@ -27,7 +29,7 @@ cd valgrind
 wget -c http://www.valgrind.org/downloads/valgrind-3.11.0.tar.bz2
 bash valgrind.SlackBuild
 cd
-bash liveslak/makemod -i /tmp/valgrind-3.11.0-x86_64-1_SBo.tgz 0067-valgrind-3.11.0-x86_64-1_SBo.sxz
+bash liveslak/$MAKEMOD -i /tmp/valgrind-3.11.0-x86_64-1_SBo.tgz 0067-valgrind-3.11.0-x86_64-1_SBo.sxz
 echo "Done valgrind module."
 echo "You can test the module contents with the command : "
 echo "unsquashfs -l 0067-valgrind-3.11.0-x86_64-1_SBo.sxz"
@@ -43,7 +45,7 @@ chmod +x $BDIR/etc/profile.d/less_custom_config.sh
 mkdir -p $BDIR/etc/rc.local
 echo "# ln -sf /etc/X11/xinit/xinitrc{.xfce,}" >> $BDIR/etc/rc.local
 echo "# loadkeys la-latin1" >> $BDIR/etc/rc.local
-bash liveslak/makemod.sh -i  $BDIR/   0068-customconfig-0.0.1-x86_64.sxz
+bash liveslak/MAKEMOD -i  $BDIR/   0068-customconfig-0.0.1-x86_64.sxz
 echo "Done config module."
 echo "You can test the module contents with the command : "
 echo "unsquashfs -l 0068-customconfig-0.0.1-x86_64.sxz"
