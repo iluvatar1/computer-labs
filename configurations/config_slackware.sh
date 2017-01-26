e061e1ade94040b92e22c0326916a7980eea1d58
Iluvatar <wfoquendop@unal.edu.co>
2017-01-25 20:00:54 -0500

    Removed +x on network manager since it was giving dns lookup errors

--- configurations/config_slackware.sh
+++ configurations/config_slackware.sh
@@ -4,9 +4,9 @@
 echo "Changing default init to 4 ..."
 sed -i.bck 's/id:3:initdefault:/id:4:initdefault:/' /etc/inittab
 
-echo "Chmod +x  network stuff ..."
-chmod +x /etc/rc.d/rc.wireless
-chmod +x /etc/rc.d/rc.networkmanager
+#echo "Chmod +x  network stuff ..."
+#chmod +x /etc/rc.d/rc.wireless
+#chmod +x /etc/rc.d/rc.networkmanager
 
 echo "Chmod +x  nfsd stuff ..."
 chmod +x /etc/rc.d/rc.nfsd
