f9038e5db54cd12cd9ccf53a5bc45e9ee1a3c19d
Iluvatar <wfoquendop@unal.edu.co>
2017-01-31 22:45:32 -0500

    Changed temporary staging dir and setting output to opt

--- liveslack/create_custom_liveslack_iso.sh
+++ liveslack/create_custom_liveslack_iso.sh
@@ -1,6 +1,6 @@
 echo "Creating liveslack iso with custom modules"
 
-STAGING_DIR=/opt/slackwarelive_staging
+STAGING_DIR=/tmp/slackwarelive_staging
 
 mount -o loop /dev/sr0 /mnt/dvd && 
 mkdir -p ${STAGING_DIR} && 
@@ -8,5 +8,5 @@ rsync -av -P --delete /mnt/dvd/ ${STAGING_DIR}/ &&
 umount /mnt/dvd && 
 cp -avu ~/Downloads/006*sxz ${STAGING_DIR}/liveslak/addons/ &&
 cd ${STAGING_DIR}/ &&
-bash ~/Downloads/liveslak/make_slackware_live.sh -G && 
+OUTPUT=/opt/ bash ~/Downloads/liveslak/make_slackware_live.sh -G && 
 echo "Done. Copy the iso where you need it."
