2f1b9270e4d9d087b188b3bf06400a849bac33d8
Iluvatar <wfoquendop@unal.edu.co>
2017-02-02 06:57:15 -0500

    Improved cleaning of modules and removed custom config mod since it was giving problemas

--- liveslack/make_extra_modules.sh
+++ liveslack/make_extra_modules.sh
@@ -2,13 +2,14 @@ echo "This script is meant to be run on a Slackware 14.2 installation where the 
 
 
 MAKEMOD=makemod
-LIVESLACKBDIR=~/Downloads/liveslak/
+LIVESLACKBDIR=/root/Downloads/liveslak/
+MODDIR=/root/Downloads/mods_liveslack
 
 cd $LIVESLACKBDIR
 git pull
 cd
 
-cd ~/Downloads
+cd $MODDIR
 if [ ! -f 0066-anaconda2-4.2.0-x86_64.sxz ]; then 
     echo "#####################################"
     echo "CREATING anaconda MODULE ... "
@@ -23,6 +24,7 @@ if [ ! -f 0066-anaconda2-4.2.0-x86_64.sxz ]; then
     chmod +x /tmp/modtemp/etc/profile.d/anaconda.sh
     echo "Creating liveslack module ... "
     bash $LIVESLACKBDIR/$MAKEMOD -i  /tmp/modtemp/ 0066-anaconda2-4.2.0-x86_64.sxz
+    rm -rf /tmp/modtemp
     echo "Done anaconda module."
     echo "You can test the module contents with the command : "
     echo "unsquashfs -l 0066-anaconda2-4.2.0-x86_64.sxz"
@@ -31,7 +33,7 @@ if [ ! -f 0066-anaconda2-4.2.0-x86_64.sxz ]; then
 fi
 
 BNAME=valgrind-3.12.0-x86_64-1_SBo
-cd ~/Downloads
+cd $MODDIR
 if [ ! -f 0067-${BNAME}.sxz ]; then
     echo "#####################################"
     echo "CREATING valgrind MODULE"
@@ -44,8 +46,9 @@ if [ ! -f 0067-${BNAME}.sxz ]; then
 	    wget -c http://www.valgrind.org/downloads/valgrind-3.12.0.tar.bz2 &&
 	    bash valgrind.SlackBuild
     fi
-    cd ~/Downloads
+    cd $MODDIR
     bash $LIVESLACKBDIR/$MAKEMOD -i /tmp/${BNAME}.tgz 0067-${BNAME}.sxz 
+    rm -rf /tmp/valgrind
     echo "Done valgrind module."
     echo "You can test the module contents with the command : "
     echo "unsquashfs -l 0067-${BNAME}.sxz"
@@ -53,24 +56,25 @@ if [ ! -f 0067-${BNAME}.sxz ]; then
     echo 
 fi
 
-BNAME=customconfig-0.0.1-x86_64
-cd ~/Downloads
-if [ ! -f 0068-${BNAME}.sxz ]; then
-    echo "#####################################"
-    echo "CREATING config MODULE"
-    BDIR=$(mktemp -d -p /tmp/)
-    mkdir -p $BDIR/etc/profile.d/
-    echo "export LESS='-eRX' " >> $BDIR/etc/profile.d/custom_config.sh
-    chmod +x $BDIR/etc/profile.d/less_custom_config.sh
-    #mkdir -p $BDIR/etc/rc.local
-    echo "# ln -sf /etc/X11/xinit/xinitrc{.xfce,}" >> $BDIR/etc/profile.d/custom_config.sh
-    echo "# loadkeys la-latin1" >> $BDIR/etc/profile.d/custom_config.sh
-    chmod +x $BDIR/etc/profile.d/custom_config.sh
-    bash $LIVESLACKBDIR/$MAKEMOD -i  $BDIR/   0068-customconfig-0.0.1-x86_64.sxz
-    echo "Done config module."
-    echo "You can test the module contents with the command : "
-    echo "unsquashfs -l 0068-customconfig-0.0.1-x86_64.sxz"
-    echo "#####################################"
-    echo 
-fi
+# BNAME=customconfig-0.0.1-x86_64
+# cd $MODDIR
+# if [ ! -f 0068-${BNAME}.sxz ]; then
+#     echo "#####################################"
+#     echo "CREATING config MODULE"
+#     BDIR=$(mktemp -d -p /tmp/)
+#     mkdir -p $BDIR/etc/profile.d/
+#     echo "export LESS='-eRX' " >> $BDIR/etc/profile.d/custom_config.sh
+#     chmod +x $BDIR/etc/profile.d/less_custom_config.sh
+#     #mkdir -p $BDIR/etc/rc.local
+#     echo "# ln -sf /etc/X11/xinit/xinitrc{.xfce,}" >> $BDIR/etc/profile.d/custom_config.sh
+#     echo "# loadkeys la-latin1" >> $BDIR/etc/profile.d/custom_config.sh
+#     chmod +x $BDIR/etc/profile.d/custom_config.sh
+#     bash $LIVESLACKBDIR/$MAKEMOD -i  $BDIR/   0068-customconfig-0.0.1-x86_64.sxz
+#     rm -rf $BDIR
+#     echo "Done config module."
+#     echo "You can test the module contents with the command : "
+#     echo "unsquashfs -l 0068-customconfig-0.0.1-x86_64.sxz"
+#     echo "#####################################"
+#     echo 
+# fi
     
