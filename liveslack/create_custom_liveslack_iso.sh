784e6bbcb1f0831f2af139482a7e830b612da021
Iluvatar <wfoquendop@unal.edu.co>
2017-02-02 06:34:00 -0500

    Minor organization of liveslack modules

--- liveslack/create_custom_liveslack_iso.sh
+++ liveslack/create_custom_liveslack_iso.sh
@@ -2,11 +2,13 @@ echo "Creating liveslack iso with custom modules"
 
 STAGING_DIR=/tmp/slackwarelive_staging
 
+rm -rf ${STAGING_DIR}
+
 mount -o loop /dev/sr0 /mnt/dvd && 
 mkdir -p ${STAGING_DIR} && 
 rsync -av -P --delete /mnt/dvd/ ${STAGING_DIR}/ && 
 umount /mnt/dvd && 
-cp -avu ~/Downloads/006*sxz ${STAGING_DIR}/liveslak/addons/ &&
+rsync -av -P --delete  ~/Downloads/mods_liveslack/ ${STAGING_DIR}/liveslak/addons/ &&
 cd ${STAGING_DIR}/ &&
 OUTPUT=/opt/ bash ~/Downloads/liveslak/make_slackware_live.sh -G && 
 echo "Done. Copy the iso where you need it."
