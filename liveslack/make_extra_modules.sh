46c5dcfa385ae130c414a0640f1d8c872ab4335b
Iluvatar <wfoquendop@unal.edu.co>
2017-08-09 18:55:43 -0500

    Updated docs to remove redundant info and centralizing all commands on the script

--- liveslack/make_extra_modules.sh
+++ liveslack/make_extra_modules.sh
@@ -11,31 +11,51 @@ git pull
 cd
 
 cd $MODDIR
-if [ ! -f 0066-anaconda2-4.3.1-x86_64.sxz ]; then 
+BNAME=Miniconda2-latest-Linux-x86_64
+if [ ! -f 0066-$BNAME.sxz ]; then 
     echo "#####################################"
-    echo "CREATING anaconda MODULE ... "
+    echo "CREATING miniconda MODULE ... "
+
+    if [ ! -d /opt/miniconda2 ]; then
+	echo "Installing miniconda and packages ...."
+	echo "Downloading ..."
+	wget https://repo.continuum.io/miniconda/$BNAME.sh -O ~/Downloads/$BNAME.sh
+	echo "Installing ..."
+	bash $BNAME.sh
+	echo "Installing/upgrading some packages ..."
+	echo "Updating conda"
+	conda update -y conda
+	echo "Installing vpython"
+	conda install -y -c vpython vpython
+	echo "Installing other packages"
+	conda install -y matplotlib scipy numpy sympy seaborn  
+	echo "Updating ipython"
+	conda install -y ipython
+	echo "Done installing miniconda"
+    fi
+    echo "Creating temp dir (if it does not exist) ..." 
     if [ ! -d /tmp/modtemp/opt ]; then 
 	mkdir -p /tmp/modtemp/opt
     fi
-    echo "Copying anaconda2 to /tmp/modtemp/opt/ ..."
-    cp -auvf /opt/anaconda2 /tmp/modtemp/opt/
+    echo "Copying miniconda2 to /tmp/modtemp/opt/ ..."
+    cp -auvf /opt/miniconda2 /tmp/modtemp/opt/
     echo "Creating a local profile to modify the path for all users ... "
     mkdir -p /tmp/modtemp/etc/profile.d
     echo 'export PATH=/usr/local/bin/:$PATH"' > /tmp/modtemp/etc/profile.d/anaconda.sh
     echo 'for a in {de,}activate anaconda conda ipython{,2} jupyter{,-notebook} pip{,2} python{,2}  ; do ln -sf /opt/anaconda2/bin/$a /usr/local/bin/ &> /dev/null; done ' > /tmp/modtemp/etc/profile.d/anaconda.sh
     chmod +x /tmp/modtemp/etc/profile.d/anaconda.sh
 
-    echo "Creating liveslack module ... "
-    bash $LIVESLACKBDIR/$MAKEMOD -i  /tmp/modtemp/ 0066-anaconda2-4.3.1-x86_64.sxz
+    echo "Creating miniconda liveslack module ... "
+    bash $LIVESLACKBDIR/$MAKEMOD -i  /tmp/modtemp/ 0066-$BNAME.sxz
     rm -rf /tmp/modtemp
-    echo "Done anaconda module."
+    echo "Done miniconda module."
     echo "You can test the module contents with the command : "
-    echo "unsquashfs -l 0066-anaconda2-4.3.1-x86_64.sxz"
+    echo "unsquashfs -l 0066-$BNAME.sxz"
     echo "#####################################"
     echo
 fi
 
-BNAME=valgrind-3.12.0-x86_64-1_SBo
+BNAME=valgrind-3.13.0-x86_64-1_SBo
 cd $MODDIR
 if [ ! -f 0067-${BNAME}.sxz ]; then
     echo "#####################################"
@@ -46,7 +66,7 @@ if [ ! -f 0067-${BNAME}.sxz ]; then
 	    wget -c https://slackbuilds.org/slackbuilds/14.2/development/valgrind.tar.gz &&
 	    tar xf valgrind.tar.gz &&
 	    cd valgrind &&
-	    wget -c http://www.valgrind.org/downloads/valgrind-3.12.0.tar.bz2 &&
+	    wget -c http://www.valgrind.org/downloads/valgrind-3.13.0.tar.bz2 &&
 	    bash valgrind.SlackBuild
     fi
     cd $MODDIR
