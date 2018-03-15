69ea167bbcb173c9e5e581ba68493a7923b74fcd
Iluvatar <woquendo@gmail.com>
2018-03-14 23:56:21 -0500

    Added more extensions to generic install

--- liveslack/make_extra_modules.sh
+++ liveslack/make_extra_modules.sh
@@ -81,7 +81,7 @@ function create_generic {
 	          mkdir -p /tmp/${NAME} &&
 	              cd /tmp/${NAME} &&
 	              wget -c ${SLACKBUILD_URL} &&
-	              tar xvf ${NAME}.tar.gz &&
+	              tar xvf ${NAME}.tar.{gz,bz2} &&
 	              cd ${NAME} &&
 	              wget -c ${SOURCE_URL} && 
 	              bash ${NAME}.SlackBuild
