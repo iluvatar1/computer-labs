722ecd22119d7109acbb27a7f435df48dbeaa466
Iluvatar <woquendo@gmail.com>
2018-03-17 22:13:23 -0500

    Deleted update files since they are going to be configured on the baisc info

--- computer-room/client/config/files/cron/update_slackware.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-slackpkg -batch=on -default_answer=y update gpg
-slackpkg -batch=on -default_answer=y update
-slackpkg -batch=on -default_answer=y upgrade-all
--- computer-room/server/cron/daily/slackpkg-sbopkg-update-upgrade.sh
+++
@@ -1,6 +0,0 @@
-# slackpkg
-slackpkg -batch=on -default_answer=y update
-slackpkg -batch=on -default_answer=y upgrade-all
-
-# sbopkg
-sbopkg -r
