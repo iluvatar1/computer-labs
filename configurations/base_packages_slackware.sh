ebf17ee6f34854abfa0735e6fba3148ab8d56bd4
Iluvatar <woquendo@gmail.com>
2018-03-18 08:38:49 -0500

    Blacklisted coreutils for slackpkg

--- configurations/base_packages_slackware.sh
+++ configurations/base_packages_slackware.sh
@@ -13,6 +13,7 @@ slackpkg -batch=on -default_answer=y update gpg
 slackpkg -batch=on -default_answer=y update
 slackpkg -batch=on -default_answer=y blacklist kernel
 slackpkg -batch=on -default_answer=y blacklist bash
+slackpkg -batch=on -default_answer=y blacklist coreutils
 #slackpkg -batch=on -default_answer=y blacklist glibc
 
 echo "Installing sbopkg ..."
