5e8c207e29ca1484c58505be4aa0feb33e169294
Iluvatar <woquendo@gmail.com>
2019-08-09 11:01:15 -0500

    More essential packages

--- packages/install_packages_ubuntu.sh
+++ packages/install_packages_ubuntu.sh
@@ -8,17 +8,17 @@ for pkg in ${PACKAGES[*]}; do
 done 
 
 function check_package_installed {
+    pkg_status=1
     if [ x"" != x"$(which $1)" ]; then
 	echo "    Already installed : $1"
 	pkg_status=0
     fi
-    pkg_status=1
 }
 
 echo "Configuring extra packages:"
 echo " -> keepassxc ..."
 check_package_installed keepassxc
-if [ 0 == $pkg_status ] ; then  
+if [ 1 == $pkg_status ] ; then  
     echo "HERE"
     read
     sudo add-apt-repository ppa:phoerious/keepassxc # do only once
@@ -28,21 +28,21 @@ fi
 # See: https://www.zerotier.com/download/
 echo " -> zerotier-one ..."
 check_package_installed zerotier-one
-if [ 0 == $pkg_status ] ; then  
+if [ 1 == $pkg_status ] ; then  
     curl -s https://install.zerotier.com | sudo bash
 fi
 
 # See: https://www.dropbox.com/install-linux
 echo " -> Dropbox ..."
 check_package_installed ~/.dropbox-dist/dropboxd
-if [ 0 == $pkg_status ] ; then  
+if [ 1 == $pkg_status ] ; then  
     cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
     ~/.dropbox-dist/dropboxd
 fi
 
 # Chrome:
 check_package_installed google-chrome
-if [ 0 == $pkg_status ] ; then  
+if [ 1 == $pkg_status ] ; then  
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 echo " -> google-chrome ..."
     sudo dpkg -i google-chrome-stable_current_amd64.deb
@@ -55,8 +55,7 @@ fi
 # sudo cp "/usr/lib/systemd/system/syncthing@.service" /etc/systemd/system/
 echo " -> syncthing ..."
 check_package_installed syncthing
-if [ 0 == $pkg_status ] ; then  
-    echo "Here"
+if [ 1 == $pkg_status ] ; then  
     # Add the release PGP keys:
     curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
     # Add the "stable" channel to your APT sources:
@@ -66,3 +65,12 @@ if [ 0 == $pkg_status ] ; then
     sudo apt install syncthing
 fi
 
+# xfce4 screensaver
+# see:
+echo " -> xfce4-screensaver ..."
+check_package_installed xfce4-screensaver
+if [ 1 == $pkg_status ]; then  
+    sudo add-apt-repository ppa:xubuntu-dev/experimental
+    sudo apt-get update
+    sudo apt-get install xfce4-screensaver
+fi 
