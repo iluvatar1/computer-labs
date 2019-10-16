e0776291e9f2fb09d600cb210142aab697a6774d
Iluvatar <woquendo@gmail.com>
2019-10-15 21:38:28 -0500

    Added crontab to check network every minute

--- computer-room/server/cron/5min/network.sh
+++ computer-room/server/cron/5min/network.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-stat=$(/sbin/ifconfig eth0 | grep 168.176); 
-if [ "" == "$stat" ]; then 
-#    /sbin/ifup eth1
-    /sbin/ifconfig eth0 down
-    /sbin/ifconfig eth0 up
+date +%c
+echo "Testing interfaces ..."
+stat=$(/sbin/ifconfig | grep 168.176);
+if [ "" == "$stat" ]; then
+    echo "Restarting eth0 ..."
+    #    /sbin/ifup eth1
+    #/sbin/ifconfig eth0 down
+    #/sbin/ifconfig eth0 up
+    /etc/rc.d/rc.inet1 eth0_restart
 fi
 
-stat=$(/sbin/ifconfig eth1 | grep 192.168); 
-if [ "" == "$stat" ]; then 
-    /sbin/ifconfig eth1 down
-    /sbin/ifconfig eth1 up
+stat=$(/sbin/ifconfig | grep 192.168);
+if [ x"" == x"$stat" ]; then
+    echo "Restarting eth1 ..."
+    #/sbin/ifconfig eth1 down
+    #/sbin/ifconfig eth1 up
+    /etc/rc.d/rc.inet1 eth1_restart
 #    /sbin/ifdown eth1
 #    /sbin/ifup eth1
 #    /etc/init.d/networking stop
