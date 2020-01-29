1cb874f170250c67cde886000cabc1cb88a0c3cd
Iluvatar <woquendo@gmail.com>
2020-01-29 15:40:09 -0500

    Fixed some typos

--- computer-room/example_config.sh
+++ computer-room/example_config.sh
@@ -2,6 +2,6 @@ BASE_SERVERIP=192.168
 SERVERIP=${BASE_SERVERIP}.10.1
 NISDOMAIN=salafisnis
 PROXYUSER=fisicasop_fcbog
-PROXYPASSWD=s4l4f1s219 # Change this
+PROXYPASSWD=PASSWD # Change this
 SERVER_DOMAINNAME=serversalafis
 FORCE=0 # Force rewrite of all config even if it is already configured
