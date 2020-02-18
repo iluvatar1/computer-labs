7d581e5fe2fea5ab863635593792ccc36e5cb2fe
Your Name <you@example.com>
2020-02-18 13:57:44 -0500

    Moved several config to general config since it was not actually specific for the computer room but more general

--- configurations/config_slackware.sh
+++ configurations/config_slackware.sh
@@ -15,59 +15,159 @@ echo "Starting configuration for slackware basebox. NOTE: This scripts tries to 
 echo "##################################################"
 echo
 
-echo "Changing default init to 4 ..."
-if [ x"" == x"$(grep 'id?4?initdefault' /etc/inittab | grep -v grep)" ]; then
-    sed -i.bck 's/id:3:initdefault:/id:4:initdefault:/' /etc/inittab
-fi
-echo "Done"
+function inittab {
+    echo "Changing default init to 4 ..."
+    if [ x"" == x"$(grep 'id?4?initdefault' /etc/inittab | grep -v grep)" ]; then
+	sed -i.bck 's/id:3:initdefault:/id:4:initdefault:/' /etc/inittab
+    fi
+    echo "Done"
+}
 
-echo "Activating services for future starts: nfs ssh "
-chmod +x /etc/rc.d/rc.nfsd && /etc/rc.d/rc.nfsd start
-chmod +x /etc/rc.d/rc.sshd && /etc/rc.d/rc.sshd start
-#chmod +x /etc/rc.d/rc.wireless
-#chmod +x /etc/rc.d/rc.networkmanager
-echo "Done"
+function services_nfs_ssh {
+    echo "Activating services for future starts: nfs ssh "
+    for fname in /etc/rc.d/rc.{nfsd,sshd}; do
+	if ! -x $fname; then
+	    chmod +x $fname && $fname start
+	fi
+    done
+    echo "Done"
+}
 
-echo "Configuring timezone to Bogota ..."
-cp -f /usr/share/zoneinfo/America/Bogota /etc/localtime
+function timezone {
+    echo "Configuring timezone to Bogota ..."
+    if [ x"" != "$(diff /usr/share/zoneinfo/America/Bogota /etc/localtime)" ]; then 
+	cp -f /usr/share/zoneinfo/America/Bogota /etc/localtime
+    else
+	echo "    -> Already configured"
+    fi
+}
 
-echo "Configuring slim login manager"
-if [ x"" == x"$(grep slim /etc/rc.d/rc.4 | grep -v grep)" ]; then
-    sed -i.bck '/echo "Starting up X11 session manager..."/a \\n# start SLiM ...\nif [ -x /usr/bin/slim ]; then exec /usr/bin/slim; fi ' /etc/rc.d/rc.4
-    ln -sf /etc/X11/xinit/xinitrc.xfce /etc/X11/xinitrc
-fi
+function slim {
+    echo "Configuring slim login manager"
+    if [ x"" == x"$(grep slim /etc/rc.d/rc.4 | grep -v grep)" ]; then
+	backup_file /etc/rc.d/rc.4
+	sed -i.bck '/echo "Starting up X11 session manager..."/a \\n# start SLiM ...\nif [ -x /usr/bin/slim ]; then exec /usr/bin/slim; fi ' /etc/rc.d/rc.4
+	ln -sf /etc/X11/xinit/xinitrc.xfce /etc/X11/xinitrc
+    fi
+}
 
-echo "Configuring slackpkg mirror"
-bfile=/etc/slackpkg/mirrors
-if [ x"1" != x"$(wc -l $bfile)" ]; then 
-    cp /etc/slackpkg/mirrors{,bck}
-    echo "http://slackware.mirrors.tds.net/pub/slackware/slackware-14.2/" > /etc/slackpkg/mirrors
-else
-    echo "    -> tds mirror already configured."
-fi
+function slackpkgmirror {
+    echo "Configuring slackpkg mirror"
+    bfile=/etc/slackpkg/mirrors
+    if [ x"1" != x"$(wc -l $bfile)" ]; then 
+	backup_file /etc/slackpkg/mirrors
+	echo "http://mirrors.slackware.com/slackware/slackware64-14.2/" > /etc/slackpkg/mirrors
+    else
+	echo "    -> mirror already configured."
+    fi
+}
 
-echo "Adding dhcp for eth1"
-sed -i.bck 's/USE_DHCP\[1\]=""/USE_DHCP\[1\]="yes"/' /etc/rc.d/rc.inet1.conf
+function dhcp_eth1 {
+    echo "Adding dhcp for eth1"
+    sed -i.bck 's/USE_DHCP\[1\]=""/USE_DHCP\[1\]="yes"/' /etc/rc.d/rc.inet1.conf
+}
 
-# Configuring lilo
-echo "Configuring lilo delay time to 5 seconds ..."
-bname="/etc/lilo.conf"
-if [ x"" == x"$(grep -re 'timeout.*50' 2>/dev/null $bname)" ]; then
-    backup_file $bname
-    sed -i.bck 's/timeout = 1200/timeout = 50/' $bname
-    lilo
-else
-    echo "   -> already configured."
-fi
+function lilo {
+    # Configuring lilo
+    echo "Configuring lilo delay time to 5 seconds ..."
+    bname="/etc/lilo.conf"
+    if [ x"" == x"$(grep -re 'timeout.*50' 2>/dev/null $bname)" ]; then
+	backup_file $bname
+	sed -i.bck 's/timeout = 1200/timeout = 50/' $bname
+	lilo
+    else
+	echo "   -> already configured."
+    fi
+}
 
-echo "Configuring cronjob for updating slackware"
-bname="update_slackware"
-if [ ! -f /etc/cron.d/${bname}_cronjob ] || [ ! -f /root/scripts/${bname}.sh ]; then
-    mkdir -p /root/scripts 2>/dev/null
-    cp -f ${bname}.sh /root/scripts/
-    cp -f ${bname}_cronjob /etc/cron.d/
-else
-    echo "    -> already configured ."
-fi
+function cron_update_slackware {
+    echo "Configuring cronjob for updating slackware"
+    bname="update_slackware"
+    if [ ! -f /etc/cron.d/${bname}_cronjob ] || [ ! -f /root/scripts/${bname}.sh ]; then
+	mkdir -p /root/scripts 2>/dev/null
+	cp -f ${bname}.sh /root/scripts/
+	cp -f ${bname}_cronjob /etc/cron.d/
+    else
+	echo "    -> already configured ."
+    fi
+}
+
+function slpkg_aux {
+    echo "You might need to setup a proxy ..."
+    sleep 5
+    VERSION=$1
+    cd ~/Downloads
+    source /root/.bashrc
+    wget -c https://gitlab.com/dslackw/slpkg/-/archive/$VERSION/slpkg-$VERSION.tar.gz
+    tar xf slpkg-$VERSION.tar.gz
+    cd slpkg-$VERSION
+    ./install.sh
+}
+function slpkg_install {
+    MSG="Installing slpkg"
+    echo "$MSG"
+    ##### installation #####
+    if hash slpkg 2>/dev/null ; then
+	echo "#    -> already installed"
+    else
+	if hash python3 2>/dev/null && hash pip3 2>/dev/null; then
+	    pip3 install urllib3
+	    slpkg_aux 3.8.2
+	else # python 2
+	    slpkg_aux 3.4.3
+	fi
+    fi
+    #### configuration ####
+    fname=/etc/slpkg/slpkg.conf
+    if [ x"" = x"$(grep 'DEFAULT_ANSWER=y' $fname | grep -v grep)" ]; then 
+	backup_file /etc/slpkg/slpkg.conf
+	sed -i.bck 's/DEFAULT_ANSWER=n/DEFAULT_ANSWER=y/' /etc/slpkg/slpkg.conf
+	sed -i.bck 's/DOWNDER_OPTIONS=.*/DOWNDER_OPTIONS=-c -N --no-check-certificate/' /etc/slpkg/slpkg.conf
+	backup_file /etc/slpkg/blacklist 
+	cat <<EOF > /etc/slpkg/blacklist
+      kernel-firmware	
+      kernel-generic	
+      kernel-generic-smp	
+      kernel-headers		
+      kernel-huge		
+      kernel-huge-smp		
+      kernel-modules		
+      kernel-modules-smp	
+      kernel-source		
+      
+      mozilla-firefox
+EOF
+    else
+	echo "       Already configured "
+    fi
+    
+    slpkg upgrade
+    echo "DONE: $MSG"
+}
+
+function dhcpcd_clientid {
+    # This is useful for some dhpc servers that don understand the ipv6 stuff
+    MSG="Configuring dhcpcd to send the mac to the dhcp server"
+    echo "$MSG"
+    fname=/etc/dhcpcd.conf
+    if [ x"" = x"$(grep '^#duid' $fname | grep -v grep)" ] || [ x"" != x"$(grep '#clientid' $fname | grep -v grep)" ] ; then
+	backup_file $fname
+	sed -i "s/^#clientid/clientid/" $fname
+	sed -i "s/^duid/#duid/" $fname
+    else
+	echo "Already configured"
+    fi
+}
+
+#inittab
+#services_nfs_ssh
+#timezone
+#slim
+#slackpkgmirror
+#dhcp_eth1
+#lilo
+#cron_update_slackware
+slpkg_install
+#dhcpcd_clientid
 
 echo "Done."
