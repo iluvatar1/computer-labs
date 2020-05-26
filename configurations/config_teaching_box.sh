8e70662b8afb537b167f1933a66c53b338783d7c
Iluvatar <woquendo@gmail.com>
2020-05-26 16:39:35 -0500

    Improved package install by using a binary repo
    For the future, it would be useful to have a build machine

--- configurations/config_teaching_box.sh
+++ configurations/config_teaching_box.sh
@@ -9,20 +9,29 @@ SPACK_PKGS=(gsl)
 ###############################################################################
 # Functions definitions
 ###############################################################################
+install_binary_packages () {
+    BASEURL="http://157.245.132.188/PACKAGES/slackware64-current/"
+    cd /tmp || exit
+    wget "$BASEURL/PACKAGES.txt" 2> /dev/null
+    while read -r line; do
+	echo "Installing: $line"
+	wget "$BASEURL/$line"
+	installpkg "$line"
+    done < PACKAGES.txt
+}
+
 install_with_slpkg () {
     rm -f /var/log/slpkg.log
     slpkg update >> /var/log/slpkg.log
     for pkg in "${SLPKG_PKGS[@]}"; do
-	# First check if package can be downloaded from droplet
-
-	# Package must be compiled
+	echo "Installing: $pkg"
 	slpkg -s sbo "$pkg" >> /var/log/slpkg.log
     done
 }
 
 install_with_spack () {
     for pkg in "${SPACK_PKGS[@]}"; do
-	spack install "$pkg"
+	sudo -u live spack install "$pkg"
     done
 }
 
@@ -59,10 +68,9 @@ install_perf () {
     MSG="Installing perf ..."
     echo "$MSG"
     sleep 2
-
     if ! command perf 2>/dev/null; then
 	cd /usr/src/linux/tools/perf || exit
-	make -j 2 prefix=/usr/local install
+	VERSION=$(uname -r) make -j 2 prefix=/usr/local install
 	echo "Done"
     else
 	echo "Already installed"
@@ -94,10 +102,14 @@ install_latest_firefox() {
 # Main 
 ###############################################################################
 rm -f /var/log/log-install.txt 2>/dev/null
-bash config_slackware.sh >> /var/log/log-install.txt
-check_live_user >> /var/log/log-install.txt
-install_with_slpkg >> /var/log/log-install.txt
-install_spack >> /var/log/log-install.txt
-install_with_spack >> /var/log/log-install.txt
-install_perf >> /var/log/log-install.txt
-install_latest_firefox >> /var/log/log-install.txt
+{
+    bash config_slackware.sh 
+    bash /etc/rc.d/rc.inet1 restart 
+    check_live_user
+    bash install_binary_packages
+    install_with_slpkg 
+    install_spack 
+    install_with_spack 
+    install_perf 
+    install_latest_firefox 
+} &>> /var/log/log-install.txt
