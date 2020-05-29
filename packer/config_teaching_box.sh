f7ef4cf514e48a94db85b1d98bd3ece7e6136d63
Iluvatar <woquendo@gmail.com>
2020-05-28 21:04:08 -0500

    Using config_teaching_box.sh from configurations

--- packer/config_teaching_box.sh
+++ packer/config_teaching_box.sh
@@ -1,18 +1,1 @@
-#!/bin/sh
-
-set -x
-set -e
-
-# Install the virtualbox additions
-mount -o loop VBoxGuestAdditions.iso /mnt/
-cd /mnt
-./VBoxLinuxAdditions.run || echo "Some error on VBox Linux Add install, but we will continue ..."
-cd /
-umount /mnt
-
-# Config the machine
-mkdir /root/repos 2> /dev/null
-cd /root/repos || exit
-git clone https://github.com/iluvatar1/computer-labs
-cd computer-labs/configurations
-bash config_teaching_box.sh
+../configurations/config_teaching_box.sh
