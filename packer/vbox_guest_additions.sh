43cef6a61ffe8070f915de7e41b2a0be599c4723
Iluvatar <woquendo@gmail.com>
2020-09-02 15:31:42 -0500

    Improved guest additions install script to use already existing file

--- packer/vbox_guest_additions.sh
+++ packer/vbox_guest_additions.sh
@@ -2,12 +2,16 @@
 install_virtualbox_guest_additions () {
     echo "Installing virtualbox guest additions"
     if ! hash vboxmanage &> /dev/null; then
-        VBOX_ADD=VirtualBox-6.1.12-139181-Linux_amd64.run
-        if [ ! -f ${VBOX_ADD} ]; then
-	        wget -c http://download.virtualbox.org/virtualbox/6.1.12/${VBOX_ADD}
-        fi
-        yes y | bash ${VBOX_ADD}
-        #rm ${VBOX_ADD}
+        TMPDIR=(mktemp -d)
+        mount -o loop ~/VBoxGuestAdditions.iso ${TMPDIR}
+        yes yes | ${TMPDIR}/VBoxLinuxAdditions.run --nox11 --nochown
+
+        # VBOX_ADD=VirtualBox-6.1.12-139181-Linux_amd64.run
+        # if [ ! -f ${VBOX_ADD} ]; then
+	    #     wget -c http://download.virtualbox.org/virtualbox/6.1.12/${VBOX_ADD}
+        # fi
+        # yes yes | bash ${VBOX_ADD}
+        # #rm ${VBOX_ADD}
     else
         echo "    -> already installed"
     fi  
