3ada0d6619f5dfface6b0da7aaa96c8fbe78a056
Iluvatar <woquendo@gmail.com>
2021-01-16 22:20:34 -0500

    Added install of spefici version with slpkg

--- packages/install_packages_slackware.sh
+++ packages/install_packages_slackware.sh
@@ -49,6 +49,21 @@ install_with_slpkg_compile () {
     done
 }
 
+aux_slbuild () {
+    cd /tmp
+    wget "$1"
+    wget "$2"
+    slname="$(basename $1)"
+    pkgname="$(basename $2)"
+    slpkg -a "$slname" "$pkgname"
+    unset $VERSION
+}
+
+install_from_source_new_version () {
+    export VERSION=6.1.0
+    aux_slbuild https://slackbuilds.org/slackbuilds/14.2/academic/octave.tar.gz  https://mirror.cedia.org.ec/gnu/octave/octave-6.1.0.tar.lz
+}
+
 install_latest_firefox() {
     MSG="Installing latest firefox ..."
     pm "$MSG"
@@ -89,6 +104,8 @@ install_perf () {
 # MAIN
 
 setup
+# install qt5 from slack
+slpkg -s slack qt5 icu4c
 
 if [ "NO" = "$COMPILE" ]; then
     install_binary_packages
@@ -100,4 +117,5 @@ fi
 install_perf
 # install some big packages already compiled by alien
 slpkg -s alien libreoffice inkscape vlc poppler-compat
+
 rm -f /tmp/*tgz 2>/dev/null
