7871a569d1bd30a4688bc842c0ab4d08ac5d4f57
Iluvatar <woquendo@gmail.com>
2021-04-25 20:51:45 -0500

    Minor fix

--- configurations/config_teaching_box.sh
+++ configurations/config_teaching_box.sh
@@ -1,16 +1,12 @@
 #!/bin/env bash
 
 ###############################################################################
-# Functions definitions
-###############################################################################
-source config_functions.sh
-
-
-###############################################################################
 # Main 
 ###############################################################################
 rm -f /var/log/log-install.txt 2>/dev/null
 {
+    BDIR="$HOME/repos/computer-labs/"
+    source $BDIR/config_functions.sh
     # Configure teaching box
     check_live_user
     sudo -u live bash install_and_setup_doom_emacs.sh
