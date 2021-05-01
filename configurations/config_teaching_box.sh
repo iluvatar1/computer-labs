2fda25db64fc477b6349fef753cd60cbb83d7b45
Iluvatar <woquendo@gmail.com>
2021-05-01 12:08:15 -0500

    Added full path for emacs doom config

--- configurations/config_teaching_box.sh
+++ configurations/config_teaching_box.sh
@@ -9,7 +9,7 @@ rm -f /var/log/log-teaching.txt 2>/dev/null
     source $BDIR/configurations/config_functions.sh
     # Configure teaching box
     check_live_user
-    sudo -u live bash install_and_setup_doom_emacs.sh
+    sudo -u live bash "$BDIR/configurations/install_and_setup_doom_emacs.sh"
     config_shell_prompt
     #sudo -u live bash fix_tz_xfce4.sh
     install_spack
