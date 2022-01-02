f076ba79078771bfe7c837c74d20fe2bfb04255e
Iluvatar <woquendo@gmail.com>
2022-01-01 23:26:35 -0500

    Test to be deleted

--- configurations/config_teaching_box.sh
+++ configurations/config_teaching_box.sh
@@ -8,6 +8,7 @@ rm -f /var/log/log-teaching.txt 2>/dev/null
     BDIR="$HOME/repos/computer-labs/"
     source $BDIR/configurations/config_functions.sh
     # Configure teaching box
+    sleep 10
     check_live_user
     cp "$BDIR/configurations/install_and_setup_doom_emacs.sh" /tmp/
     sudo -u live bash "/tmp/install_and_setup_doom_emacs.sh"
