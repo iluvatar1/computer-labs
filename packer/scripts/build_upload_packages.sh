2b159111016b451ca83a4134e695caf408e16ec2
root <you@example.com>
2022-01-05 07:28:24 -0500

    Moved from LOGNAME to LOGBNAME var name since LOGNAME already exists on the bash env

--- packer/scripts/build_upload_packages.sh
+++ packer/scripts/build_upload_packages.sh
@@ -2,12 +2,12 @@
 set -euo pipefail
 
 MNAME=${MNAME:-base_slack_machine} 
-LOGNAME=${LOGNAME:-build}
+LOGBNAME=${LOGBNAME:-build}
 BUILDFILE=${BUILDFILE:-NULL.json} # can use other provision files
 
 BDIR=$(dirname $(realpath $0))
 
 echo "Building and uploading packages " && \
-PACKER_LOG=1 packer build -var MACHINENAME=${MNAME} -var CPUS=7 -var PROXY="http://192.168.10.1:3128/" -var TSNAPSHOT=${LOGNAME} -var-file ${BUILDFILE} "${BDIR}/../"slackware64-current-D-build_packages.json &> /tmp/log-D-$LOGNAME.txt && \
+PACKER_LOG=1 packer build -var MACHINENAME=${MNAME} -var CPUS=7 -var PROXY="http://192.168.10.1:3128/" -var TSNAPSHOT=${LOGNAME} -var-file ${BUILDFILE} "${BDIR}/../"slackware64-current-D-build_packages.json &> /tmp/log-D-$LOGBNAME.txt && \
 echo "Finished."
 
