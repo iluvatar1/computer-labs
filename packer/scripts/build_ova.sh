85bfb4fc7128ec51922e0a281fb63a94484f624a
root <you@example.com>
2022-01-05 13:03:15 -0500

    Moved instructions to create the base machine to different script. Now the ova uses it, and the build packages use it also

--- packer/scripts/build_ova.sh
+++ packer/scripts/build_ova.sh
@@ -2,23 +2,16 @@
 set -euo pipefail
 
 MNAME=${MNAME:-base_slack_machine}
-LOGNAME=${LOGNAME:-teaching}
+LOGBNAME=${LOGBNAME:-teaching}
 PROVFILE=${PROVFILE:-provision_vars-teaching.json} # can use other provision files
+PROXY=${PROXY:-}
 
 BDIR=$(dirname $(realpath $0))
 
-echo "Creating base machine in virtualbox" && \
-FIRMWARE=bios MACHINENAME=$MNAME bash "${BDIR}/"create_base_machine.sh && \
-echo "Installing slackware system and packages" && \
-PACKER_LOG=1 packer build -var ISO_URL=$HOME/Downloads/iso/slackware64-current-install-dvd.iso -var MACHINENAME=${MNAME} -var CPUS=2 "${BDIR}/../"slackware64-current-A-install.json &> /tmp/log-A-$LOGNAME.txt && \
-echo "Finishing slackware install and configuration" && \
-PACKER_LOG=1 packer build -var MACHINENAME=${MNAME} "${BDIR}/../"slackware64-current-B-finish_install.json &> /tmp/log-B-$LOGNAME.txt && \
-echo "Setting up machine with smaller lilo time etc" && \
-PACKER_LOG=1 packer build -var MACHINENAME=${MNAME} "${BDIR}/../"slackware64-current-C-initial_setup.json &> /tmp/log-C-$LOGNAME.txt && \
 echo "Installing packages " && \
-PACKER_LOG=1 packer build -var MACHINENAME=${MNAME} "${BDIR}/../"slackware64-current-D-install_packages.json &> /tmp/log-D-$LOGNAME.txt && \
-echo "Provisioning and exporting $LOGNAME machine" && \
-PACKER_LOG=1 packer build -var-file ${PROVFILE} -var MACHINENAME=${MNAME} "${BDIR}/../"slackware64-current-E-provision.json &> /tmp/log-E-$LOGNAME.txt && \
+PACKER_LOG=1 packer build -var MACHINENAME=${MNAME} -var PROXY="$PROXY" "${BDIR}/../"slackware64-current-D-install_packages.json &> /tmp/log-D-$LOGBNAME.txt && \
+echo "Provisioning and exporting $LOGBNAME machine" && \
+PACKER_LOG=1 packer build -var-file ${PROVFILE} -var MACHINENAME=${MNAME} "${BDIR}/../"slackware64-current-E-provision.json &> /tmp/log-E-$LOGBNAME.txt && \
 echo "Finished."
 
 #        [ "modifyvm", "{{.Name}}", "--firmware", "efi"],
---
+++ packer/scripts/create_config_slack_machine.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+MNAME=${MNAME:-base_slack_machine}
+LOGBNAME=${LOGBNAME:-teaching}
+PROXY=${PROXY:-}
+
+BDIR=$(dirname $(realpath $0))
+
+echo "Creating base machine in virtualbox" && \
+FIRMWARE=bios MACHINENAME=$MNAME bash "${BDIR}/"create_vbox_machine.sh && \
+echo "Installing slackware system and packages" && \
+PACKER_LOG=1 packer build -var ISO_URL=$HOME/Downloads/iso/slackware64-current-install-dvd.iso -var MACHINENAME=${MNAME} -var CPUS=2 "${BDIR}/../"slackware64-current-A-install.json &> /tmp/log-A-$LOGBNAME.txt && \
+echo "Finishing slackware install and configuration" && \
+PACKER_LOG=1 packer build -var MACHINENAME=${MNAME} "${BDIR}/../"slackware64-current-B-finish_install.json &> /tmp/log-B-$LOGBNAME.txt && \
+echo "Setting up machine with smaller lilo time etc" && \
+PACKER_LOG=1 packer build -var MACHINENAME=${MNAME} -var PROXY="$PROXY" "${BDIR}/../"slackware64-current-C-initial_setup.json &> /tmp/log-C-$LOGBNAME.txt && \
+echo "Finished."
+
+#        [ "modifyvm", "{{.Name}}", "--firmware", "efi"],
+#/usr/local/bin/VBoxManage modifyvm base_slack_machine --firmware bios #&& \
