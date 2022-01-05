fd6ff416b0257c6e8d985665eb7ac23b574c7c42
root <you@example.com>
2022-01-05 09:45:08 -0500

    Fixed LOGNAME for LOGBNAME

--- packer/scripts/build_upload_packages.sh
+++ packer/scripts/build_upload_packages.sh
@@ -8,6 +8,6 @@ BUILDFILE=${BUILDFILE:-NULL.json} # can use other provision files
 BDIR=$(dirname $(realpath $0))
 
 echo "Building and uploading packages " && \
-PACKER_LOG=1 packer build -var MACHINENAME=${MNAME} -var CPUS=7 -var PROXY="http://192.168.10.1:3128/" -var TSNAPSHOT=${LOGNAME} -var-file ${BUILDFILE} "${BDIR}/../"slackware64-current-D-build_packages.json &> /tmp/log-D-$LOGBNAME.txt && \
+PACKER_LOG=1 packer build -var MACHINENAME=${MNAME} -var CPUS=7 -var PROXY="http://192.168.10.1:3128/" -var TSNAPSHOT=${LOGBNAME} -var-file ${BUILDFILE} "${BDIR}/../"slackware64-current-D-build_packages.json &> /tmp/log-D-$LOGBNAME.txt && \
 echo "Finished."
 
