aca72aa3f802cef58ab6d3efe6e40afa166c5f58
root <you@example.com>
2022-01-05 19:18:23 -0500

    Added example to config server

--- packer/scripts/build_ova_computer_room.sh
+++ packer/scripts/build_ova_computer_room.sh
@@ -5,14 +5,17 @@ set -euo pipefail
 # LOGBNAME=client PROVFILE=../config/provision_vars-client.json PROXY=http://192.168.10.1:3128 bash build_ova_client.sh
 
 MNAME=${MNAME:-base_slack_machine}
-LOGBNAME=${LOGBNAME:-client}
+LOGBNAME=${LOGBNAME:-client} # or server
 PROVFILE=${PROVFILE:-provision_vars-client.json} # can use other provision files
 #PROXY=${PROXY:-}
 
 BDIR=$(dirname $(realpath $0))
 
+# NOTE: Pacages assumed to be already installed 
 #echo "Installing packages " && \
-#PACKER_LOG=1 packer build -var MACHINENAME=${MNAME} -var PROXY="$PROXY" "${BDIR}/../"slackware64-current-D-install_packages.json &> /tmp/log-D-$LOGBNAME.txt && \
+    #PACKER_LOG=1 packer build -var MACHINENAME=${MNAME} -var PROXY="$PROXY" "${BDIR}/../"slackware64-current-D-install_packages.json &> /tmp/log-D-$LOGBNAME.txt && \
+
+
 echo "Provisioning and exporting $LOGBNAME machine" && \
 PACKER_LOG=1 packer build -var-file ${PROVFILE} -var MACHINENAME=${MNAME} "${BDIR}/../"slackware64-current-E-provision.json &> /tmp/log-E-$LOGBNAME.txt && \
 echo "Finished."
