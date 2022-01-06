d27623aeb7fff0b0f9abd62f749b780a26d644ac
root <you@example.com>
2022-01-05 19:10:13 -0500

    Updated config for computer room using the same script for both client and server

--- configurations/config_computer_room_box.sh
+++ configurations/config_computer_room_box.sh
@@ -3,16 +3,24 @@
 ###############################################################################
 # Main 
 ###############################################################################
-rm -f /tmp/log-client.txt 2>/dev/null
+TYPE=${1}
+if [[ x"CLIENT" != x"${TYPE}" ]] && [[ x"SERVER" != x"${TYPE}" ]]; then
+    echo "ERROR. Usage : "
+    echo "$0 TYPE"
+    echo "where TYPE=CLIENT|SERVER"
+    exit 1
+fi
+
+rm -f /tmp/log-${TYPE}.txt 2>/dev/null
 {
     BDIR="$HOME/repos/computer-labs/"
     source $BDIR/configurations/config_functions.sh
     clone_or_update_config_repo
-    # configure as client
+    # configure as TYPE
     cd "$BDIR/computer-room"
     # tangle the config file
     emacs --batch -l org config_computer_room.org -f org-babel-tangle
     # run the config script
     cd scripts
-    ALL=1 bash bootstrap_slackware_computer_room.sh ../files CLIENT
-} 2>&1 | tee /tmp/log-client.txt
+    ALL=1 bash bootstrap_slackware_computer_room.sh ../files $TYPE
+} 2>&1 | tee /tmp/log-$TYPE.txt
