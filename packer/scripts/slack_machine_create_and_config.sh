8561999bfbd1bc7dd4b8665d39ddb31fc85e256e
root <you@example.com>
2022-01-08 16:45:13 -0500

    Removed old not used strings

--- packer/scripts/slack_machine_create_and_config.sh
+++ packer/scripts/slack_machine_create_and_config.sh
@@ -2,7 +2,6 @@
 set -euo pipefail
 
 MNAME=${MNAME:-base_slack_machine}
-LOGBNAME=${LOGBNAME:-teaching}
 PROXY=${PROXY:-}
 
 BDIR=$(dirname $(realpath $0))
@@ -10,11 +9,11 @@ BDIR=$(dirname $(realpath $0))
 echo "Creating base machine in virtualbox" && \
 FIRMWARE=bios MACHINENAME=$MNAME bash "${BDIR}/"create_vbox_machine.sh && \
 echo "Installing slackware system and packages" && \
-PACKER_LOG=1 packer build -var ISO_URL=$HOME/Downloads/iso/slackware64-current-install-dvd.iso -var MACHINENAME=${MNAME} -var CPUS=2 "${BDIR}/../"slackware64-current-A-install.json &> /tmp/log-A-$LOGBNAME.txt && \
+PACKER_LOG=1 packer build -var ISO_URL=$HOME/Downloads/iso/slackware64-current-install-dvd.iso -var MACHINENAME=${MNAME} -var CPUS=2 "${BDIR}/../"slackware64-current-A-install.json &> /tmp/log-A.txt && \
 echo "Finishing slackware install and configuration" && \
-PACKER_LOG=1 packer build -var MACHINENAME=${MNAME} "${BDIR}/../"slackware64-current-B-finish_install.json &> /tmp/log-B-$LOGBNAME.txt && \
+PACKER_LOG=1 packer build -var MACHINENAME=${MNAME} "${BDIR}/../"slackware64-current-B-finish_install.json &> /tmp/log-B.txt && \
 echo "Setting up machine with smaller lilo time etc" && \
-PACKER_LOG=1 packer build -var MACHINENAME=${MNAME} -var PROXY="$PROXY" "${BDIR}/../"slackware64-current-C-initial_setup.json &> /tmp/log-C-$LOGBNAME.txt && \
+PACKER_LOG=1 packer build -var MACHINENAME=${MNAME} -var PROXY="$PROXY" "${BDIR}/../"slackware64-current-C-initial_setup.json &> /tmp/log-C.txt && \
 echo "Finished."
 
 #        [ "modifyvm", "{{.Name}}", "--firmware", "efi"],
