d63f1c9478ae253dda8c0888a220272a19021bfe
root <you@example.com>
2022-01-31 08:48:20 -0500

    Moved xdm as first option

--- configurations/config_slackware.sh
+++ configurations/config_slackware.sh
@@ -355,6 +355,15 @@ config_hostname() {
     echo localhost > /etc/HOSTNAME
 }
 
+config_hostname() {
+    pm "Configuring xdm as first option in runlevel 4 ..."
+    if [ x"" = x"$(grep 'xdm' /etc/rc.d/rc.4.local 2>/dev/null | grep -v grep)" ]; then
+	echo "if [ -x /usr/bin/xdm ]; then exec /usr/bin/xdm -nodaemon; elif [ -x /usr/X11R6/bin/xdm ]; then exec /usr/X11R6/bin/xdm -nodaemon; fi" > /etc/rc.d/rc.4.local
+    else
+	    configured 'xdm as first option'
+    fi
+}
+
 #####################################################
 # MAIN
 #####################################################
@@ -391,4 +400,5 @@ slackpkgmirror
 config_fonts
 config_xwmconfig
 config_hostname
+config_xdm
 pm "Done."
