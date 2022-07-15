fdca785eab89251c5931d2727b6b233c8641f590
root <you@example.com>
2022-07-15 16:42:53 -0500

    Added more quota info

--- ansible/computer_room/server/files/etc/cron.daily/daily_maintenance.sh
+++ ansible/computer_room/server/files/etc/cron.daily/daily_maintenance.sh
@@ -6,17 +6,16 @@
 #slpkg -s slack "" --patches
 
 # Re-assing quota to all users and then check it
-QUOTA_HARDLIMIT=5.5G
-QUOTA_SOFTLIMIT=6.0G
+QUOTA_SOFTLIMIT=5120000
+QUOTA_HARDLIMIT=5632000
 for a in $(cat /etc/passwd | awk '{ FS=":";if ($3 > 1000) print $1}'); do
     if [ -d /home/$a ]; then
-	echo $a
-	if [[ "$a" == "oquendo" ]] || [[ "$a" == "ramezquitao" ]] || [[ "$a" == "ersanchezp" ]]; then
-	    /usr/sbin/setquota -b -u $a 0 0
-	else
-	    echo /usr/sbin/setquota -b -u $a $QUOTA_HARDLIMIT $QUOTA_SOFTLIMIT
-	    echo 2
-	fi
+	    echo $a
+	    if [[ "$a" == "oquendo" ]] || [[ "$a" == "ramezquitao" ]] || [[ "$a" == "ersanchezp" ]] || [[ "$a" == "cesandovalu" ]]; then
+            /usr/sbin/setquota -u $a 0 0  0 0 /home
+	    else
+            /usr/sbin/setquota -u $a 5120000 5632000  0 0 /home
+	    fi
     fi
 done
 /usr/sbin/quotacheck -vug /home
