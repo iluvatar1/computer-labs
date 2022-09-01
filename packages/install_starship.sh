953e1788efbfab354f7bb03b45cd31f73b596f7d
root <you@example.com>
2022-09-01 15:10:02 -0500

    Improved install starship script

--- packages/install_starship.sh
+++ packages/install_starship.sh
@@ -3,3 +3,5 @@ cd /tmp
 wget https://starship.rs/install.sh
 sh install.sh -y
 echo 'eval "$(starship init bash)"' >> /root/.bashrc
+echo 'eval "$(starship init bash)"' > /etc/profile.d/starship.sh
+chmod +x /etc/profile.d/starship.sh
