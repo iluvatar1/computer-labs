8e795d9ed7f05c9394ec7dfa9565c820e6dc8dfe
root <you@example.com>
2022-09-01 13:30:57 -0500

    Added upgrade slackware script using slackpkg

---
+++ packages/upgrade_slackware.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+source /root/.bashrc
+echo YES | slackpkg -batch=on -default_answer=y update gpg
+slackpkg  -batch=on -default_answer=y update
+slackpkg  -batch=on -default_answer=y install-new
+slackpkg  -batch=on -default_answer=y upgrade-all
+
+slpkg upgrade
+slpkg -s alien upgrade libreoffice inkscape vlc
+
+# Fix x2go
+#perl -MCPAN -e 'recompile()'
+#perl -MCPAN -e 'install DBD::SQLite'
+#sbopkg -r
+#removepkg /var/log/packages/perl-DBD-SQLite*
+#echo 'P' | MAKEFLAGS=-j$(nproc) sbopkg -i perl-DBD-SQLite
+# OR
+#removepkg /var/log/packages/x2goserver*
+#qg x2goserver
