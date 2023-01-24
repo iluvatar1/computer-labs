aaca3c14ecc8f0fcec11e33f06c5e10cb06e27a0
root <you@example.com>
2023-01-23 20:28:42 -0500

    Fixed typo on profile script and added timeout to task

--- ansible/computer_room/packages_server/files/etc/profile.d/spack_setup.sh
+++ ansible/computer_room/packages_server/files/etc/profile.d/spack_setup.sh
@@ -10,7 +10,7 @@ NC='\033[0m' # No Color
 
 echo -e "${YELLOW}Sourcing spack ... (trying for 5 seconds) ${NC}"
 timeout 5s stat /packages/spack/share/spack/setup-env.sh &>/dev/null
-if [ "$?" == "0" ]; then 
+if [[ "$?" == "0" ]]; then
     source /packages/spack/share/spack/setup-env.sh
     echo -e "${GREEN}spack ready.${NC}"
 else
