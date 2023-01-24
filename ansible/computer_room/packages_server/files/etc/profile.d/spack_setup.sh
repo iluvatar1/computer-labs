a2ecd59945da474f397bfcc7a9bcb5c388187eca
root <you@example.com>
2023-01-23 19:44:55 -0500

    Improved profile loading for spack packages when the nfs package server is stalled. Now it is first, checking the file to load with a timeout and then loading it if found or printing an error message

--- ansible/computer_room/packages_server/files/etc/profile.d/spack_setup.sh
+++ ansible/computer_room/packages_server/files/etc/profile.d/spack_setup.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-if [ -f /packages/spack/share/spack/setup-env.sh ]; then
+#if [ -f /packages/spack/share/spack/setup-env.sh ]; then
+#    source /packages/spack/share/spack/setup-env.sh
+#fi
+
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+echo -e "${YELLOW}Sourcing spack ... (trying for 5 seconds) ${NC}"
+timeout 5s stat /packages/spack/share/spack/setup-env.sh &>/dev/null
+if [ "$?" == "0" ]; then 
     source /packages/spack/share/spack/setup-env.sh
+    echo -e "${GREEN}spack ready.${NC}"
+else
+    echo -e "${RED}ERROR:Problem sourcing spack from the packages server. ${NC}"
 fi
