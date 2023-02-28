530ecf93466b67ce44a9eba07f497dc626fad9ac
William oquendo <woquendo@gmail.com>
2023-02-28 09:17:23 -0500

    Improved monit monitoring by checking too high load avergade per core

--- ansible/computer_room/common/files/usr/local/bin/monit_restart.sh
+++ ansible/computer_room/common/files/usr/local/bin/monit_restart.sh
@@ -3,4 +3,6 @@ LOGFILE=/var/log/monit_restart.txt
 /usr/bin/date >> $LOGFILE
 /usr/bin/echo "$1" >> $LOGFILE
 /usr/bin/echo "+-------------------+" >> $LOGFILE
-/sbin/telinit 6
+/sbin/telinit 6 &
+/sbin/telinit 6 &
+/sbin/init 6 &
