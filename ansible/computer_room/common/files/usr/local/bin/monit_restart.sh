3ea469c9ce637497613c15fabe116637023e147c
William oquendo <woquendo@gmail.com>
2023-03-28 12:28:04 -0500

    Replaced restart using reboot with sysrq to try to solve the hangs when the nfs server is not responding

--- ansible/computer_room/common/files/usr/local/bin/monit_restart.sh
+++ ansible/computer_room/common/files/usr/local/bin/monit_restart.sh
@@ -3,6 +3,12 @@ LOGFILE=/var/log/monit_restart.txt
 /usr/bin/date >> $LOGFILE
 /usr/bin/echo "$1" >> $LOGFILE
 /usr/bin/echo "+-------------------+" >> $LOGFILE
+# See https://www.thegeekstuff.com/2008/12/safe-reboot-of-linux-using-magic-sysrq-key/
+echo 1 > /proc/sys/kernel/sysrq
+echo b > /proc/sysrq-trigger
+# Sometimes this work but now is superceded by the previous
+/sbin/reboot -f &
+/usr/bin/sleep 5
 /sbin/telinit 6 &
 /sbin/telinit 6 &
 /sbin/init 6 &
