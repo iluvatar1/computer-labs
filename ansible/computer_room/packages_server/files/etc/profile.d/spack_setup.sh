d09a545af1cac53240bc49c36dd591edc37a1033
William oquendo <woquendo@gmail.com>
2023-12-14 20:47:08 -0500

    Added view to environment to specify location, and updated ansible spack config on clients

--- ansible/computer_room/packages_server/files/etc/profile.d/spack_setup.sh
+++ ansible/computer_room/packages_server/files/etc/profile.d/spack_setup.sh
@@ -16,6 +16,7 @@ echo -e "${YELLOW}Sourcing spack ... (trying for 5 seconds) ${NC}"
 timeout 5s stat /packages/spack/share/spack/setup-env.sh &>/dev/null
 if [[ "$?" == "0" ]]; then
     source /packages/spack/share/spack/setup-env.sh
+    spack env activate salafis
     echo -e "${GREEN}spack ready.${NC}"
 else
     echo -e "${RED}ERROR:Problem sourcing spack from the packages server. ${NC}"
