ee99f08f9ff3f190032eff4c4fb9dc32e6bfd827
William oquendo <woquendo@gmail.com>
2024-03-15 22:28:54 -0500

    Removed check that was not allowing the root to login

--- ansible/computer_room/common/roles/motd/files/etc/profile.d/motd_upd.sh
+++ ansible/computer_room/common/roles/motd/files/etc/profile.d/motd_upd.sh
@@ -3,9 +3,9 @@
 cat /etc/slackware-version
 uname -s -r
 
-if [[ "root" == "$(whoami)" ]]; then
-    return
-fi
+#if [[ "root" == "$(whoami)" ]]; then
+#    exit 0
+#fi
 
 echo "En este momento se encuentran activos los siguientes cientes:"
 cat /home/clients_status.txt
