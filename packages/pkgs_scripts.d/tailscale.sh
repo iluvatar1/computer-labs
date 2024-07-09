2877823572b336cdfa3aa1dc54a27f994360d78c
William oquendo <woquendo@gmail.com>
2024-07-08 21:49:46 -0500

    Added latest tailscale version

--- packages/pkgs_scripts.d/tailscale.sh
+++ packages/pkgs_scripts.d/tailscale.sh
@@ -1,6 +1,6 @@
 AUXDIR=$(mktemp -d)
 cd $AUXPDIR
-wget -c https://pkgs.tailscale.com/stable/tailscale_1.36.0_amd64.tgz
+wget -c https://pkgs.tailscale.com/stable/tailscale_1.68.1_amd64.tgz
 tar xvf tailscale*tgz
 cp tailscale*/tailscale /usr/local/bin/
 cp tailscale*/tailscaled /usr/local/bin/
