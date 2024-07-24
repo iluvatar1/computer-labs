972aa21c52bc0fa9f187332bc92828b9ffd7e852
William oquendo <woquendo@gmail.com>
2024-07-23 21:24:20 -0500

    Added x2goserver script to build and install it using sbo. Working

---
+++ packages/pkgs_scripts.d/install_x2goserver.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+printf "Q\nP\n" | MAKEFLAGS=-j$(nproc) sbopkg -i x2goserver
+
+# check if binary is working
+x2godbadmin --createdb &>/dev/null
+if [[ "$?" == "0" ]]; then
+    echo "x2go seems to be working. Exiting"
+    exit 0
+fi
+
+# remove old packages
+removepkg /var/log/packages/perl-DBD-SQLite-*
+removepkg /var/log/packages/nxlibs-*
+removepkg /var/log/packages/x2goserver-*
+
+# update sbo
+sbopkg -r
+
+# create queue
+sqg -p xegoserver
+
+# build and install package
+printf "Q\nP\n" | MAKEFLAGS=-j$(nproc) sbopkg -i x2goserver
+
+# test
+x2godbadmin --createdb &>/dev/null
+echo "Status after running x2godbadmin: $?"
+
+
+
